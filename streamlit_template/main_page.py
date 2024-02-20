eb9d6ec1fd8b5503abca00534a698c31afb56c18
mattk3011 <mattk3011@gmail.com>
2024-02-20 15:10:56 -0700

    Updating links

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -5,5 +5,5 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer stats")
 
-link = '[To my Github Pages site](http://mattk.github.io/github_assignment/)'
+link = '[To my Github Pages site](http://127.0.0.1:5500/)'
 st.markdown(link, unsafe_allow_html=True)
